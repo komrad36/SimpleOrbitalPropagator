69d8a5ba17ac7dca6b2d7bfe40ee8d985070c768
komrad36 <kareem.omar@uah.edu>
2015-09-23 19:15:06 -0500

    Added screenshot

--- OrbitPropagator.m
+++ OrbitPropagator.m
@@ -1,5 +1,5 @@
 [~, pv] = ode45(@(~, pv) [pv(4:6); -398600/(norm(pv(1:3)))^3.*pv(1:3)],...
-    [0 1e5], [10000 0 500 0 9 0]', odeset('RelTol', 1e-8));
+    [0 1e5], [7000 0 500 0 9 0]', odeset('RelTol', 1e-8));
 %   [time ]  [initial pos, vel ] in km and km/s
 hold on, grid on
 plot3(0,0,0, 'bo', 'MarkerFaceColor', 'b', 'MarkerSize', 40)
